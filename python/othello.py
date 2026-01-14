c4ac48a87ee4bf7e0cf339f1b0e70631fd9b0d8a
Daniel Willett - Mint Laptop <willettdr1@gmail.com>
2026-01-14 16:51:31 +0000

    Current solution is a little bit jank but at least it works. Players can now take it in turns placing moves into the grid. So far it only checks that the position isn't already occupied. Next task is 'legal' moves which in Othello translates to the actual moves of the game

--- python/othello.py
+++ python/othello.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 import re
 
-grid = []
-
 def setup():
     for i in range(8):
         temp = []
@@ -20,21 +18,59 @@ def display():
     print("   A  B  C  D  E  F  G  H") 
 
 def interpretInput(usrInput):
+    lookup = {"A":0,
+              "B":1,
+              "C":2,
+              "D":3,
+              "E":4,
+              "F":5,
+              "G":6,
+              "H":7}
     usrInput = usrInput.replace(" ","")
     match = re.search("^[A-H][,-;:_]*[1-8]$", usrInput)
     if match==None:
         return -1
     char = (re.search("[A-H]",usrInput)).group()
-    num = (re.search("[1-8]", usrInput)).group()
-    return [char,num]
+    x = lookup[char]
+    y = int((re.search("[1-8]", usrInput)).group())-1 #Arrays start at 0
+    y = 7-y #The display is in reverse order to the `grid` indexing
+    return [x,y]
 
-setup()
+def makeMove(position):
+    global player
+    x = position[0]
+    y = position[1]
+    if grid[y][x]!=0:
+        print("This position is already taken. Please choose an empty position")
+        player -= 1 #The next step is to increment it to the next player but we want the current player to try again
+    else:
+        grid[y][x]=player
+
+#Initialise
+grid = []
+"""
+8 [0, 0, 0, 0, 0, 0, 0, 0]
+7 [0, 0, 0, 0, 0, 0, 0, 0]
+6 [0, 0, 0, 0, 0, 0, 0, 0]
+5 [0, 0, 0, 1, 2, 0, 0, 0]
+4 [0, 0, 0, 2, 1, 0, 0, 0]
+3 [0, 0, 0, 0, 0, 0, 0, 0]
+2 [0, 0, 0, 0, 0, 0, 0, 0]
+1 [0, 0, 0, 0, 0, 0, 0, 0]
+   A  B  C  D  E  F  G  H
+"""
 gameOver = False
 player = 1
+
+
+setup()
 while gameOver==False:
     display()
+    print("Player "+str(player)+"'s turn:")
     usrInput = input("where would you like to place?\n")
     position = interpretInput(usrInput)
     if position==-1:
         print("======Invalid input======")
-        continue
+    else:
+        makeMove(position)
+        player = (player%2)+1 #Switch between (0,1) but scaled by 1 so switch between (1,2)
